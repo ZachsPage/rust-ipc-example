36533c1312462cf363c3621e00b33508b269f162
Zach <xhmf1423@gmail.com>
2022-01-29 23:24:11 -0500

    Launch procs in bg and await exit

--- src/bin/proc_1.rs
+++ src/bin/proc_1.rs
@@ -1,5 +1,8 @@
 use ipc_comms;
+use std::env;
 
 fn main() {
     ipc_comms::shared_fcn("proc_1")
+    let args: Vec<String> = env::args().collect();
+    println!("Got args {:?}", args);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::process::Command;
+use std::process::{Command, Child};
+use std::io::Result;
+use std::slice;
 use ipc_comms;
 
-fn launch_proc_and_print_output(name : &str) {
+fn launch_bkg_proc(name : &str, args : Option<&[&str]>) -> Result<Child> {
     let exe_path = ipc_comms::get_dir_of_curr_exe().expect("Failed to get path");
     let proc_to_launch = exe_path.join(name);
-    let proc_res = Command::new(proc_to_launch).output().expect("Failed to launch");
-    let output_string = std::str::from_utf8(&proc_res.stdout).expect("Failed to get output");
-    println!("{}", output_string);
+    let mut proc = Command::new(proc_to_launch);
+    match args {
+        Some(args) => for arg in args { proc.arg(arg); },
+        _ => ()
+    }
+    proc.spawn()
 }
 
 fn main() {
-    println!("Hello, world!");
-    launch_proc_and_print_output("proc_1");
-    launch_proc_and_print_output("proc_2");
+    let pipe_name = "pipe_name";
+    let proc_args = Option::Some(slice::from_ref(&pipe_name));
+    let mut proc_1 = launch_bkg_proc("proc_1", proc_args).expect("Failed proc_1");
+    let mut proc_2 = launch_bkg_proc("proc_2", proc_args).expect("Failed proc_1");
+
+    println!("Awaiting child processes to exit...");
+    proc_1.wait().expect("Failed waiting on proc_1");
+    proc_2.wait().expect("Failed waiting on proc_2");
+    println!("Both child processes exited!");
 }
